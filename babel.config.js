491b354afcb5b0b595d13b61c7db82bd63fe394f
melih <nmelih@gmail.com>
2026-01-28 13:04:57 +0300

    fix: remove seperate worklet package since reanimated last version includes it

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
 
-  // react-native-worklets/plugin has to be listed last.
-  plugins: ['react-native-worklets/plugin'],
+  plugins: ['react-native-reanimated/plugin'],
 };
