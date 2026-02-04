a7a55efc4cfd749a2e6c9bc00ba57945f9012ee0
melih <nmelih@gmail.com>
2026-02-04 15:34:58 +0300

    fix[android]: remove new architecture entrypoint load

--- android/app/src/main/java/com/nurimelih/blockpuzzle/MainApplication.kt
+++ android/app/src/main/java/com/nurimelih/blockpuzzle/MainApplication.kt
@@ -7,7 +7,6 @@ import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 
@@ -33,9 +32,6 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      load()
-    }
     loadReactNative(this)
   }
 }
