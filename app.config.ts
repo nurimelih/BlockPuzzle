b3362e0d7cf769792581fe45bbc6d0ea160cc447
melih <nmelih@gmail.com>
2026-02-23 02:51:10 +0300

    feat: add expo ota update

---
+++ app.config.ts
@@ -0,0 +1,37 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Block Puzzle',
+  slug: 'block-puzzle',
+  version: '1.0.0',
+  orientation: 'portrait',
+  ios: {
+    bundleIdentifier: 'com.nurimelih.blockpuzzle',
+    buildNumber: '1',
+  },
+  android: {
+    package: 'com.nurimelih.blockpuzzle',
+    versionCode: 1,
+  },
+  extra: {
+    eas: {
+      projectId: '10f43049-6f71-4498-a61b-f384fb000dbe',
+    },
+  },
+  updates: {
+    url: 'https://u.expo.dev/10f43049-6f71-4498-a61b-f384fb000dbe',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+  plugins: [
+    'expo-localization',
+    [
+      'expo-updates',
+      {
+        username: 'nurimelih',
+      },
+    ],
+  ],
+});
