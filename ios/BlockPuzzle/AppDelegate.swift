97f893eceb87deeeb689a111b3526db3f47f5caf
melih <nmelih@gmail.com>
2026-02-23 03:38:41 +0300

    fix: use correct Expo.plist location from Supporting folder

--- ios/BlockPuzzle/AppDelegate.swift
+++ ios/BlockPuzzle/AppDelegate.swift
@@ -2,7 +2,6 @@ import UIKit
 import React
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
-import EXUpdates
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
-    #if !DEBUG
-    let controller = AppController.initializeWithoutStarting()
-    controller.start()
-    #endif
-
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
@@ -48,7 +42,7 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 #if DEBUG
     RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
 #else
-    AppController.sharedInstance.launchAssetURL()
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
   }
 }
