2476c59a873485444484f021b6a056be73b48d82
melih <nmelih@gmail.com>
2026-03-03 20:02:22 +0300

    fix: Initialize AppController in DEBUG before RN start to work

--- ios/BlockPuzzle/AppDelegate.swift
+++ ios/BlockPuzzle/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     #if DEBUG
+    AppController.initializeWithoutStarting()
     startReactNative()
     #else
     AppController.initializeWithoutStarting()
