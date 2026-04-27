bce928c8e77c478f3a8c481e0ee2b4e626f9bb65
melih <nmelih@gmail.com>
2026-04-27 17:54:35 +0300

    Bump version to 1.0.1; add Codemagic cache

--- app.config.ts
+++ app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: 'Block Puzzle',
   slug: 'block-puzzle',
-  version: '1.0.0',
+  version: '1.0.1',
   orientation: 'portrait',
   ios: {
     bundleIdentifier: 'com.nurimelih.blockpuzzle',
